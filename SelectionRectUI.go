df0f922a755ef8aae692f7ceb7afaf35ea64b98d
hinst <hinstance@yandex.ru>
2018-02-20 23:15:43 +0300

    Added SelectionRectUI class

---
+++ SelectionRectUI.go
@@ -0,0 +1,40 @@
+package hebiten
+
+import (
+	"github.com/hajimehoshi/ebiten"
+)
+
+type SelectionRectUI struct {
+	BackgroundImage AtlasTexture
+	Target          *ebiten.Image
+	Pendulum        Pendulum
+	Color           TFloatColor
+}
+
+func (this *SelectionRectUI) Create() *SelectionRectUI {
+	this.Pendulum.Bottom = 0.5
+	this.Pendulum.Top = 1
+	this.Pendulum.Speed = 0.5
+	if this.Color.CheckIfZero() {
+		this.Color = this.GetDefaultColor()
+	}
+	return this
+}
+
+func (this *SelectionRectUI) GetDefaultColor() TFloatColor {
+	return TFloatColor{R: 128, G: 191, B: 255, A: 255}.Divide(255)
+}
+
+func (this *SelectionRectUI) Draw(rect TFloatRect) {
+	var selectionRect TDraw
+	selectionRect.Target = this.Target
+	selectionRect.AtlasTexture = this.BackgroundImage
+	selectionRect.Position = rect.GetLT()
+	selectionRect.Size = rect.GetSize()
+	selectionRect.Mask = this.Color.SetAlpha(this.Pendulum.Value)
+	selectionRect.Draw()
+}
+
+func (this *SelectionRectUI) Update(deltaTime float64) {
+	this.Pendulum.Update(deltaTime)
+}
