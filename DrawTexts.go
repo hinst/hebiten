4f2205deed02101c94a98154d07a472f588d37a5
hinst <hinstance@yandex.ru>
2018-03-18 23:34:38 +0300

    DrawTexts: no longer fixed-length. Now has dynamic length, so no need to guess size when initializing it

--- DrawTexts.go
+++ DrawTexts.go
@@ -9,29 +9,36 @@ import (
 )
 
 type DrawTexts struct {
-	Target *ebiten.Image
-	Font   font.Face
-	List   []DrawText
-	count  int
+	Target     *ebiten.Image
+	Font       font.Face
+	List       []DrawText
+	count      int
+	CountLimit int
 }
 
-func (this *DrawTexts) EnsureLength(length int) {
-	if len(this.List) != length {
-		this.List = make([]DrawText, length)
+func (this *DrawTexts) Create() *DrawTexts {
+	if this.CountLimit == 0 {
+		this.CountLimit = 1000
 	}
+	return this
 }
 
-func (this *DrawTexts) Reset(length int) {
-	this.EnsureLength(length)
+func (this *DrawTexts) Reset() {
 	this.count = 0
 }
 
 func (this *DrawTexts) Add(item DrawText) {
-	var length = len(this.List)
 	var count = this.count
-	hgo.AssertWT(count < length,
-		func() string { return "Length exceeded; l=" + hgo.IntToStr(length) + " c=" + hgo.IntToStr(count) })
-	this.List[count] = item
+	var countLimit = this.CountLimit
+	hgo.AssertWT(count < this.CountLimit, func() string {
+		return "Count exceeded; count=" + hgo.IntToStr(count) + " countLimit=" + hgo.IntToStr(countLimit)
+	})
+	var length = len(this.List)
+	if count < length {
+		this.List[count] = item
+	} else {
+		this.List = append(this.List, item)
+	}
 	this.count++
 }
 
@@ -40,6 +47,7 @@ func (this *DrawTexts) Draw() {
 		if this.count <= i {
 			break
 		}
+		hgo.Assert(this.Target != nil)
 		ebiten_text.Draw(this.Target, drawText.Text, this.Font, drawText.X, drawText.Y, drawText.Color)
 		GlobalDrawNumber++
 	}
